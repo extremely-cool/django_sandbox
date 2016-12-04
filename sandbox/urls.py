04b45e36a788eee0df7f91e05335fbfca9d58264
extremely <ip.disabled@gmail.com>
2016-12-05 00:09:49 +0300

    new styles

--- sandbox/urls.py
+++ sandbox/urls.py
@@ -4,4 +4,10 @@ from . import views
 
 urlpatterns = [
     url(r'^$', views.post_list, name='scoreboard'),
+    url(r'^scoreboard', views.post_list, name='scoreboard'),
+    url(r'^injuries', views.injuries_list, name='injuries'),
+    url(r'^support', views.injuries_list, name='support'),
+    url(r'^schedule', views.injuries_list, name='schedule'),
+    url(r'^standings', views.injuries_list, name='standings'),
 ]
+
--- sandbox/views.py
+++ sandbox/views.py
@@ -6,3 +6,8 @@ from .models import Post
 def post_list(request):
     posts = Post.objects.filter(published_date__lte=timezone.now()).order_by('published_date')
     return render(request, 'blog/scoreboard.html', {'posts': posts})
+
+
+def injuries_list(request):
+    posts = Post.objects.filter(published_date__lte=timezone.now()).order_by('published_date')
+    return render(request, 'blog/injuries.html', {'posts': posts})
